c3dc5fc9228dead8b90c9497b47692736d984a2c
ralucabondoc <ralucabondoc8@gmail.com>
2025-10-29 23:09:06 +0100

    skeleton main

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Small Language Model (SLM) starting..." << std::endl;
+
+    // TODO: parse command line arguments
+    // TODO: build language model
+    // TODO: generate text
+
+    return 0;
+}
